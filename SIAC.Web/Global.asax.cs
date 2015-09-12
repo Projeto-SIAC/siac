204d58ee57065d0b32638653ee55ba10b07b75b6
felipemfp <felipemfpontes@gmail.com>
2015-09-12 16:58:11 -0300

    Opções da Dashboard

--- SIAC.Web/Global.asax.cs
+++ SIAC.Web/Global.asax.cs
@@ -15,6 +15,7 @@ namespace SIAC.Web
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
         }
     }
 }
