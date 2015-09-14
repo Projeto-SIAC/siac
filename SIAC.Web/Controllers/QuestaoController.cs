af7159f4921c000137dae33376ee56bec85954ca
felipemfp <felipemfpontes@gmail.com>
2015-09-14 00:29:52 -0300

    Confirmação de Cadastramento de Questão

--- SIAC.Web/Controllers/QuestaoController.cs
+++ SIAC.Web/Controllers/QuestaoController.cs
@@ -48,8 +48,59 @@ namespace SIAC.Web.Controllers
         // POST: Questao/Confirmar
         public ActionResult Confirmar(FormCollection formCollection)
         {
-            ViewBag.FormCollection = formCollection;
-            return View();
+            var dc = Models.DataContextSIAC.GetInstance();
+            Models.Questao questao = new Models.Questao();
+
+            questao.Professor = dc.Professor.Single(p => p.MatrProfessor == (string)Session["UsuarioMatricula"]);
+            questao.CodProfessor = questao.Professor.CodProfessor;
+
+            // Gerais
+            questao.CodDificuldade = int.Parse(formCollection["ddlDificuldade"]);
+            questao.Dificuldade = dc.Dificuldade.Single(d => d.CodDificuldade == questao.CodDificuldade);
+            questao.CodTipoQuestao = int.Parse(formCollection["ddlTipo"]);
+            questao.TipoQuestao = dc.TipoQuestao.Single(t => t.CodTipoQuestao == questao.CodTipoQuestao);
+            var codDisciplina = int.Parse(formCollection["ddlDisciplina"]);
+            var codTemas = formCollection["ddlTema"].Split(',');
+            foreach (var strCod in codTemas)
+            {
+                var codTema = int.Parse(strCod);
+                questao.QuestaoTemas.Add(new Models.QuestaoTema {
+                    CodDisciplina = codDisciplina,
+                    CodTema = codTema,
+                    Tema = dc.Tema.Single(t=>t.CodTema == codTema && t.CodDisciplina == codDisciplina)                    
+                });
+            }
+
+
+            // Detalhes
+            questao.Enunciado = formCollection["txtEnunciado"];
+            questao.Objetivo = !String.IsNullOrEmpty(formCollection["txtObjetivo"]) ? formCollection["txtObjetivo"] : null;
+
+            // Discursiva
+            if (questao.CodTipoQuestao == 2)
+            {
+                questao.ChaveDeResposta = formCollection["txtChaveDeResposta"];
+                questao.Comentario = !String.IsNullOrEmpty(formCollection["txtComentario"]) ? formCollection["txtComentario"] : null;
+            }
+
+            // Objetiva
+            if (questao.CodTipoQuestao == 1)
+            {
+                var qteAlternativas = int.Parse(formCollection["txtQtdAlternativas"]);
+                for (int i = 0; i < qteAlternativas; i++)
+                {
+                    questao.Alternativas.Add(new Models.Alternativa {
+                        CodOrdem = i,
+                        Enunciado = formCollection["txtAlternativaEnunciado" + (i + 1)],
+                        Comentario = !String.IsNullOrEmpty(formCollection["txtAlternativaComentario" + (i + 1)]) ? formCollection["txtAlternativaComentario" + (i + 1)] : null,
+                        FlagGabarito = !String.IsNullOrEmpty(formCollection["chkAlternativaCorreta" + (i + 1)]) ? true : false
+                    });
+                }
+            }
+
+            TempData["Questao"] = questao;
+
+            return View(questao);
         }
 
         [AcceptVerbs(HttpVerbs.Get)]
