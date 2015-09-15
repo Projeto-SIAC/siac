4bf2e8a59c53bb209d0a93864f5070051cae84cd
felipemfp <felipemfpontes@gmail.com>
2015-09-15 14:22:21 -0300

    Fix LeroLero + Helper StringExtensao

---
+++ SIAC.Web/Helpers/StringExtensao.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SIAC.Web
+{
+    public static class StringExtensao
+    {
+        public static string ToShortString(this string str, int length)
+        {
+            string text = string.Empty;
+
+            if (str.Length > length) { 
+                text = str.Substring(0, length);
+                string afterText = str.Substring(length);
+
+                afterText = afterText.Remove(afterText.IndexOf(' '));
+
+                text += afterText;
+
+                text += "...";
+            }
+            else
+            {
+                text = str;
+            }
+
+            return text;
+        }
+    }
+}
