2da44439bf8adeec5ecbd0ca49da90a8d9fe5cb4
felipemfp <felipemfpontes@gmail.com>
2015-10-27 21:47:21 -0300

    Resultados por Questão em Academica/Detalhe
    + Gráficos com cores dinâmicas

---
+++ SIAC.Web/Helpers/CorDinamica.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SIAC.Helpers
+{
+    public class CorDinamica
+    {
+        public static string Rgba(Random random, float opacidade = 1)
+        {
+            string rgba = String.Empty;
+            int r = random.Next(256);
+            int g = random.Next(256);
+            int b = random.Next(256);
+            rgba = String.Format("rgba({0},{1},{2},{3})", r, g, b, opacidade);
+            return rgba;
+        }
+    }
+}
