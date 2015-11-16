8c5c88f04472eae2e97a4bf97f95f641f728614c
Francisco Bento <francisco.bento.jr@hotmail.com>
2015-11-16 18:00:34 -0300

    Atualiza Cadastro de Questões em Institucional + Adiciona InstitucionalGerarQuestaoViewModel + Atualiza aviso em cadastrar questão

--- SIAC.Web/Scripts/siac.js
+++ SIAC.Web/Scripts/siac.js
@@ -112,9 +112,6 @@
         else if (/\/historico\/avaliacao\/certificacao\/agendada\/cert[0-9]+$/.test(pathname)) {
             siac.Certificacao.Agendada.iniciar();
         }
-        else if (/\/institucional\/configurar(|\/indicador|\/categoria|\/modulo)$/.test(pathname)) {
-            siac.Institucional.Configurar.iniciar();
-        }
         else if (/\/dashboard\/avaliacao\/certificacao\/realizar\/cert[0-9]+$/.test(pathname)) {
             siac.Certificacao.Realizar.iniciar();
         }
@@ -123,6 +120,12 @@
         }
         else if (/\/dashboard\/avaliacao\/certificacao\/resultado\/cert[0-9]+$/.test(pathname)) {
             siac.Certificacao.Resultado.iniciar();
+        }
+        else if (/\/institucional\/configurar(|\/indicador|\/categoria|\/modulo)$/.test(pathname)) {
+            siac.Institucional.Configurar.iniciar();
+        }
+        else if (pathname == '/institucional/gerar') {
+            siac.Institucional.Gerar.iniciar();
         }
     }
 
