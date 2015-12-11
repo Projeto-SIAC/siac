2d19c08bfdf0b970354aeba28906306e2c337971
felipemfp <felipemfpontes@gmail.com>
2015-12-11 08:24:39 -0300

    Realiza correções pedidas pelo orientador
    - Alterar página de acesso para: "Possibilita o conhecimento do
    aprendizado"
    
    - "Nova Questão" para "Cadastrar Questão"
    
    - Adicionar descrições nos atalhos, menu e botões
    
    - "Abrir Agenda" para "Visualizar Agendamentos"
    
    - "Abrir Histórico" para "Histórico das Atividades"
    
    - "Abrir Perfil" para "Visualizar Perfil"
    
    - Colocar nos cartões rótulos dos tipos de avaliação em Correções
    Pendentes

--- SIAC.Web/Models/pTipoAvaliacao.cs
+++ SIAC.Web/Models/pTipoAvaliacao.cs
@@ -4,6 +4,8 @@ namespace SIAC.Models
 {
     public partial class TipoAvaliacao
     {
+        public string DescricaoCurta => CodTipoAvaliacao == 4 ? Descricao : Descricao.Split(' ').Last();
+
         private static dbSIACEntities contexto => Repositorio.GetInstance();
 
         public static TipoAvaliacao ListarPorCodigo(int codTipoAvaliacao) => contexto.TipoAvaliacao.FirstOrDefault(ta => ta.CodTipoAvaliacao == codTipoAvaliacao);
