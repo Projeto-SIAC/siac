fc68964eab422ca15c6a76bd6d5fd189db911951
felipemfp <felipemfpontes@gmail.com>
2015-12-18 07:13:37 -0300

    Implementa MockHelper
    Este Helper ajudará nos testes quando envolvem elementos do
    HttpContext como, por exemplo, Session.

---
+++ SIAC.Tests/Helpers/MockHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.SessionState;
+
+namespace SIAC.Tests
+{
+    public class MockHelper
+    {
+        public static HttpContext FakeHttpContext(string url)
+        {
+            var uri = new Uri(url);
+            var httpRequest = new HttpRequest(string.Empty, uri.ToString(), uri.Query.TrimStart('?'));
+            var stringWriter = new StringWriter();
+            var httpResponse = new HttpResponse(stringWriter);
+            var httpContext = new HttpContext(httpRequest, httpResponse);
+
+            var sessionContainer = new HttpSessionStateContainer("id", new SessionStateItemCollection(),
+                                                    new HttpStaticObjectsCollection(), 10, true,
+                                                    HttpCookieMode.AutoDetect,
+                                                    SessionStateMode.InProc, false);
+
+            SessionStateUtility.AddHttpSessionStateToContext(
+                                         httpContext, sessionContainer);
+
+            return httpContext;
+        }
+    }
+}
