2ea88f3b206b27c59392fc3aac158cd1a4b6feb5
felipemfp <felipemfpontes@gmail.com>
2015-12-18 06:54:33 -0300

    Implementa MockHelper
    Este Helper ajudará nos testes quando envolvem elementos do
    HttpContext como, por exemplo, Session.

---
+++ SIAC.Tests/PrincipalControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIAC.Controllers;
+using SIAC.Models;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIAC.Tests
+{
+    [TestClass]
+    public class PrincipalControllerTest
+    {
+        [TestMethod]
+        public void TestPendenteModel()
+        {
+            HttpContext.Current = MockHelper.FakeHttpContext("http://siac.apphb.com/principal/pendente");
+
+            Helpers.Sessao.Inserir("UsuarioMatricula", "20150002");
+
+            var controller = new PrincipalController();           
+
+            var result = controller.Pendente() as ViewResult;
+
+            var modelEsperado = new List<Avaliacao>();
+
+            Assert.AreEqual(modelEsperado.GetType(), result.Model.GetType());
+        }        
+    }
+}
