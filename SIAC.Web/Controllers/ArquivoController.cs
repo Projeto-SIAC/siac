0627721d4caf0d3cbcd6c333e455df29659dd258
felipemfp <felipemfpontes@gmail.com>
2016-05-03 17:46:11 -0300

    Inicia implementação do Arquivo de Simulados
    Foi feito a tela geral com Simulados abertos e encerrados.
    Está pendente as telas específicas.

---
+++ SIAC.Web/Controllers/ArquivoController.cs
@@ -0,0 +1,31 @@
+﻿using SIAC.Models;
+using SIAC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIAC.Controllers
+{
+    [Filters.AutenticacaoFilter(Categorias = new[] { Categoria.COLABORADOR, Categoria.PROFESSOR })]
+    public class ArquivoController : Controller
+    {
+        private List<Simulado> SimNaoEncerrados => Simulado.ListarNaoEncerradoOrdenadamente();
+        private List<Simulado> SimEncerrados => Simulado.ListarEncerradoOrdenadamente();
+
+        // GET: simulado/arquivo
+        public ActionResult Index() =>
+            View(new ArquivoIndexViewModel()
+            {
+                MaisAbertos = SimNaoEncerrados.Count > 3,
+                Abertos = SimNaoEncerrados.Take(3).ToList(),
+                MaisEncerrados = SimEncerrados.Count > 3,
+                Encerrados = SimEncerrados.Take(3).ToList()
+            });
+
+        // TODO: método para ser utilizado ajax para atualizar as listas em Abertos e Encerrados
+        public ActionResult Abertos() => View();
+        public ActionResult Encerrados() => View();
+    }
+}
