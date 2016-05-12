86fd26d7ad7c5787a2d0ee4fe65853245dedc446
felipemfp <felipemfpontes@gmail.com>
2016-05-12 11:16:33 -0300

    Implementa impressão de Cartão de Inscricao

---
+++ SIAC.Web/Models/SimCandidatoPartial.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SIAC.Models
+{
+    public partial class SimCandidato
+    {
+        public string NumInscricaoRepresentacao => this.NumInscricao.ToString("d4");
+    }
+}
